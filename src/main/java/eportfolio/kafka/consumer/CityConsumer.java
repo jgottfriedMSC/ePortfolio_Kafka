ce53d27d5ea0f64a890d4ec3f46ab5b85a9e77ea
jgottfriedMSC <jgottfried@mainstorconcept.com>
2021-04-20 09:53:34 +0200

    Live Demo Result

--- src/main/java/eportfolio/kafka/consumer/CityConsumer.java
+++ src/main/java/eportfolio/kafka/consumer/CityConsumer.java
@@ -1,9 +1,43 @@
 package eportfolio.kafka.consumer;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import eportfolio.kafka.model.City;
+
 public class CityConsumer {
 
 	public static void main(String[] args) throws Exception {
-		// TODO: Implement Kafka Producer here.
+		String topicName = "cities";
+		String groupName = "cityGroup";
+
+		// Consumer Configuration
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", groupName);
+		props.put("auto.offset.reset", "earliest");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "eportfolio.kafka.serializer.CityDeserializer");
+
+		KafkaConsumer<String, City> consumer = new KafkaConsumer<>(props);
+		consumer.subscribe(Arrays.asList(topicName));
+
+		while (true) {
+			ConsumerRecords<String, City> records = consumer.poll(Duration.ofMillis(100));
+			for (ConsumerRecord<String, City> record : records) {
+				System.out.println("{ City ID: " + record.value().getId() + ", City Name: " + record.value().getName()
+						+ ", City State: " + record.value().getState() + ", City Country: "
+						+ record.value().getCountry() + ", City Coordinates: [ Lat: "
+						+ record.value().getCoords().getLat() + ", Lon: " + record.value().getCoords().getLon()
+						+ " ] }");
+
+			}
+		}
 	}
 
 }
--- src/main/java/eportfolio/kafka/producer/CityProducer.java
+++ src/main/java/eportfolio/kafka/producer/CityProducer.java
@@ -1,9 +1,65 @@
 package eportfolio.kafka.producer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import eportfolio.kafka.model.City;
+import eportfolio.kafka.model.Coordinates;
+
 public class CityProducer {
 
 	public static void main(String[] args) throws Exception {
-		// TODO: Implement Kafka Producer here.
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream("cities.json");
+		JSONParser parser = new JSONParser();
+		
+		String topicName = "cities";
+		
+		// Producer Configuration
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "eportfolio.kafka.serializer.CitySerializer");
+		
+		Producer<String, City> producer = new KafkaProducer<>(props);
+		
+		try {
+			JSONArray cities = (JSONArray) parser.parse(new InputStreamReader(is));
+			
+			for (Object c : cities) {
+				JSONObject city = (JSONObject) c;
+				
+				String id = city.get("id").toString();
+				String name = (String) city.get("name");
+				String state = (String) city.get("state");
+				String country = (String) city.get("country");
+				
+				JSONObject coord = (JSONObject) city.get("coord");
+				
+				double lon = (double) coord.get("lon");
+				double lat = (double) coord.get("lat");
+				
+				Coordinates coordObj = new Coordinates(lon, lat);
+				
+				City cityObj = new City(id, name, state, country, coordObj);
+				
+				producer.send(new ProducerRecord<>(topicName, "CITY", cityObj)).get();
+			}
+			System.out.println("CityProducer completed.");
+			producer.close();
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
